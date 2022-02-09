401c0588376b6cebd42c26f67cfe253a1f227de0
Hossam <hussamaddin516@gmail.com>
2022-02-09 23:46:18 +0200

    fix controllers

--- Client/src/main/java/gov/iti/jets/client/HelloApplication.java
+++ Client/src/main/java/gov/iti/jets/client/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/views/profile.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/views/contactProfile.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setMinWidth(1050);
         stage.setMinHeight(550);
---
+++ Client/src/main/java/gov/iti/jets/client/presentation/controllers/UserProfileController.java
@@ -0,0 +1,49 @@
+package gov.iti.jets.client.presentation.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+public class UserProfileController {
+    @FXML
+    private HBox information;
+
+    @FXML
+    private HBox logout;
+
+    @FXML
+    private HBox msg;
+
+    @FXML
+    private HBox notifications;
+
+    @FXML
+    private Button update;
+
+    @FXML
+    void logoutClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showInformation(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showMsg(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showNotification(MouseEvent event) {
+
+    }
+}
