2cdbaaf46bcd3a4d3e92ea4863dc82f6ac7ec73d
heshmAhmed <heshamahmed22226.ha@gmail.com>
2022-02-10 20:56:49 +0200

    refactor module-info

--- Server/src/main/java/module-info.java
+++ Server/src/main/java/module-info.java
@@ -10,6 +10,5 @@ module gov.iti.jets.server {
 
     opens gov.iti.jets.server.presentation.controllers to javafx.fxml;
     exports gov.iti.jets.server;
-    exports gov.iti.jets.server.controllers;
-    opens gov.iti.jets.server.controllers to javafx.fxml;
+    exports gov.iti.jets.server.presentation.controllers;
 }
