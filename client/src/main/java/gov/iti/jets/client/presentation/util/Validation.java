35d6193286abf0ec8ed629809a9ef002c93ffaf6
Hossam <hussamaddin516@gmail.com>
2022-02-16 19:27:58 +0200

    validate 2.0

--- client/src/main/java/gov/iti/jets/client/presentation/util/Validation.java
+++ client/src/main/java/gov/iti/jets/client/presentation/util/Validation.java
@@ -15,7 +15,7 @@ public abstract class Validation{
     //add new contact
 
     public static boolean validateUserName(TextField userNameField , Label label){
-        if(userNameField.getText().length() <=2){
+        if(userNameField.getText().trim().length() <=2){
             label.setText("Name at least 3 characters");
             return false;
         }else{
@@ -28,7 +28,7 @@ public abstract class Validation{
 
     public static boolean validateEmail(TextField emailField , Label label) {
         String email = emailField.getText();
-        if (email.length() > 0 && !email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+        if (email.length()<0 || email.length() > 0 && !email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
             label.setText("Enter a valid Email");
             return false;
         } else {
@@ -38,7 +38,7 @@ public abstract class Validation{
     }
 
     public static boolean validatePassword(PasswordField passwordField , Label label) {
-        if (passwordField.getText().length() <=1 ) {
+        if (passwordField.getText().trim().length() <=1 ) {
             label.setText("password should be 2 characters at least");
             return false;
         }else {
@@ -48,7 +48,7 @@ public abstract class Validation{
     }
 
     public static boolean validatePhoneNumber(TextField phoneField,Label label) {
-        if (!phoneField.getText().matches("\\d{11,15}")) {
+        if (!phoneField.getText().trim().matches("\\d{11,15}")) {
             label.setText("Enter a valid number");
             return false;
         } else {
@@ -58,7 +58,7 @@ public abstract class Validation{
         }
 
     public static boolean validateConfirmPassword(PasswordField passwordField ,PasswordField confirmPasswordField, Label label) {
-        if (!passwordField.getText().equals(confirmPasswordField.getText())) {
+        if (!passwordField.getText().trim().equals(confirmPasswordField.getText())) {
             label.setText("Not matching the password field");
             return false;
         }else {
