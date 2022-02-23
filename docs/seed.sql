915a014cc4b5f31ac972a7ccba7a2927406a63e9
heshmAhmed <heshamahmed22226.ha@gmail.com>
2022-02-23 17:30:28 +0200

    update seed file

--- docs/seed.sql
+++ docs/seed.sql
@@ -1,27 +1,27 @@
 
 
- INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `password`)
+ INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `pass`)
  VALUES ('01151303667', 'hesham', 'hesham901@gmail.com', 'cccc', 'm', 'cairo', '2022-02-18', 'hello', 'active', '123456789777hgdhd');
  
- INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `password`)
+ INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `pass`)
  VALUES ('0115928056', ' Elsisi', 'elsisi@gmail.com', 'cccc', 'm', 'cairo', '2022-02-18', 'hello', 'active', '123456789777hgdhd');
  
- INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `password`)
+ INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `pass`)
  VALUES ('01555528056', 'hosaam', 'hosam1@gmail.com', 'cccc', 'm', 'cairo', '2022-02-18', 'hello', 'active', '123456789777hgdhd');
  
- INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `password`)
+ INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `pass`)
  VALUES ('01002528056', 'hatem', 'hatem@gmail.com', 'cccc', 'm', 'cairo', '2022-02-18', 'hello', 'active', '123456789777hgdhd');
- INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `password`)
+ INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `pass`)
  VALUES ('01095201234', 'shawky', 'eshawky@gmail.com', 'cccc', 'm', 'cairo', '2022-02-18', 'hello', 'active', '123456789777hgdhd');
- INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `password`)
+ INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `pass`)
  VALUES ('0109524587', 'hamdii', 'isi901@gmail.com', 'cccc', 'm', 'cairo', '2022-02-18', 'hello', 'active', '123456789777hgdhd');
- INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `password`)
+ INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `pass`)
  VALUES ('01245879114', 'MAGJ', 'eMAGJisi901@gmail.com', 'cccc', 'm', 'cairo', '2022-02-18', 'hello', 'active', '123456789777hgdhd');
- INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `password`)
+ INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `pass`)
  VALUES ('01095012457', 'essam', 'essam901@gmail.com', 'cccc', 'm', 'cairo', '2022-02-18', 'hello', 'active', '123456789777hgdhd');
- INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `password`)
+ INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `pass`)
  VALUES ('01012458888', 'tarek', 'tareki901@gmail.com', 'cccc', 'm', 'cairo', '2022-02-18', 'hello', 'active', '123456789777hgdhd');
- INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `password`)
+ INSERT INTO `chatting_application`.`users` (`phone_number`, `username`, `email`, `image`, `gender`, `country`, `date_of_birth`, `bio`, `user_status`, `pass`)
  VALUES ('0100215455', 'MElsisi', 'tarek@gmail.com', 'cccc', 'm', 'cairo', '2022-02-18', 'hello', 'active', '123456789777hgdhd');
  
  
