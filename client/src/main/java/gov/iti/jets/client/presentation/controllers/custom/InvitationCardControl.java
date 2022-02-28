b9efe3a27fb7bc66c315c64cd41c87e35ca0fd2d
heshmAhmed <heshamahmed22226.ha@gmail.com>
2022-02-28 22:49:02 +0200

    solve invitation date ui

--- client/src/main/java/gov/iti/jets/client/presentation/controllers/custom/InvitationCardControl.java
+++ client/src/main/java/gov/iti/jets/client/presentation/controllers/custom/InvitationCardControl.java
@@ -2,6 +2,7 @@ package gov.iti.jets.client.presentation.controllers.custom;
 
 import gov.iti.jets.client.network.service.ContactService;
 import gov.iti.jets.client.presentation.util.InvitationsListHelper;
+import gov.iti.jets.client.util.DateHandler;
 import gov.iti.jets.common.dtos.InvitationDTO;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
@@ -13,7 +14,8 @@ import java.io.IOException;
 
 
 public class InvitationCardControl extends HBox {
-    private final ContactService contactService = ContactService.getInstance();
+    private ContactService contactService;
+    private DateHandler dateHandler;
     private InvitationDTO invitationDTO;
 
     @FXML
@@ -24,6 +26,9 @@ public class InvitationCardControl extends HBox {
 
     @FXML
     private Label invitationBody;
+
+    @FXML
+    private Label invitationTime;
 
     public Label getInvitationBody() {
         return invitationBody;
@@ -41,8 +46,6 @@ public class InvitationCardControl extends HBox {
         this.invitationTime = invitationTime;
     }
 
-    @FXML
-    private Label invitationTime;
 
     public InvitationCardControl(InvitationDTO invitationDTO) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/invitations/InvitationCardView.fxml"));
@@ -52,13 +55,15 @@ public class InvitationCardControl extends HBox {
             loader.load();
             this.invitationDTO = invitationDTO;
             this.getInvitationBody().setText(invitationDTO.getSenderName()+": Sent an invitation");
-            this.getInvitationTime().setText(invitationDTO.getDate()+"");
+            this.getInvitationTime().setText(dateHandler.millisToLocalDate(invitationDTO.getDate()).toString());
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
     public void initialize() {
+        this.dateHandler = DateHandler.getInstance();
+        this.contactService = ContactService.getInstance();
         acceptBtn.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED,
                 e -> contactService.acceptInvitation(invitationDTO));
         denyBtn.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED,
